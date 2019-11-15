334fe0300af293e2225cf81a507315f683b699ce
David <firestix475@outlook.com>
2019-11-15 08:54:46 +0800

    viewsDone

--- app/src/main/java/com/example/listviewexample/MainActivity.java
+++ app/src/main/java/com/example/listviewexample/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Adapter;
 import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity  {
@@ -14,19 +15,23 @@ public class MainActivity extends AppCompatActivity  {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     //Todo reference listview
-
-        ListView listView = findViewById(R.id.list);
-
-
+        ListView listView = findViewById(R.id.listView);
 
     //Todo ListView listView = findViewById();
     //Todo initialize custom adapter(MyAdapter) - using  method getPlayers, pay attention to constructor of MyAdapter
-        //Todo  add custom adapter to listview
+        //Todo add custom adapter to listview
         //Todo listView.setAdapter(------);
-
+        ArrayAdapter<Player> MyAdapter = new ArrayAdapter<->(this, R.layout.activity_main, getPlayers());
+        listView.setAdapter(MyAdapter);
     }
+
     //Todo  create a method getPlayers() that will create an array of 15 players and return the list of type List<Player>
     // choose successful sportsmen from different sports, make sure to fill Player class before starting
+    public void getPlayers() {
+
+    }
+
 
 }
