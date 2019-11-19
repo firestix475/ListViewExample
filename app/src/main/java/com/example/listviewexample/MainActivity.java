32a7755aa1389bff25dc8e93fb600221c5dbb173
RonalYu <2431411779@qq.com>
2019-11-19 08:40:55 +0800

    mainActivity

--- app/src/main/java/com/example/listviewexample/MainActivity.java
+++ app/src/main/java/com/example/listviewexample/MainActivity.java
@@ -2,29 +2,55 @@ package com.example.listviewexample;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Adapter;
 import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity  {
+
+    public ArrayList<Player> list;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     //Todo reference listview
-
-
+        ListView listView = findViewById(R.id.listView);
 
     //Todo ListView listView = findViewById();
     //Todo initialize custom adapter(MyAdapter) - using  method getPlayers, pay attention to constructor of MyAdapter
         //Todo  add custom adapter to listview
         //Todo listView.setAdapter(------);
-
+        ArrayAdapter<Player>MyAdapter = new ArrayAdapter<->(this,R.layout.activity_main,getPlayers());
+        listView.setAdapter(MyAdapter);
     }
     //Todo  create a method getPlayers() that will create an array of 15 players and return the list of type List<Player>
     // choose successful sportsmen from different sports, make sure to fill Player class before starting
-
+    public Player getPlayers(){
+        String[] name = new String[] {"Tristan Thompson", "James Harden","Kyrie Irving","Stephen Curry","Russell Westbrook",
+                "Derrick Rose","Kevin Durant","Dwyane Wade","Kobe Bryant","LeBron James",
+                "Magic Johnson","Miro Jurisic","Kareem Abdul-Jabbar","Shaquille O'Neal","Chris Paul"};
+        int[] age = new int[]{28,30,27,31,30,
+                31,31,37,41,34,
+                60,18,72,47,34};
+        int[] imageResource = new int[]{R.drawable.tristanthompson,R.drawable.jamesharden,R.drawable.kyrieirving,R.drawable.stephencurry,R.drawable.russellwestbrook,
+                R.drawable.derrickrose,R.drawable.kevindurant,R.drawable.dwyanewade,R.drawable.kobebryant,R.drawable.lebronjames,
+                R.drawable.magicjohnson,R.drawable.mirojurisic,R.drawable.kareemabduljabbar,R.drawable.shaquilleoneal,R.drawable.chrispaul};
+        int[] worth = new int[]{8,120,70,130,150,
+                85,170,120,500,480,
+                600,1000,20,400,120};
+        String[] url = new String[]{"https://www.biography.com/athlete/tristan-thompson","https://www.biography.com/athlete/james-harden","https://www.biography.com/athlete/kyrie-irving","https://www.biography.com/athlete/stephen-curry","https://www.biography.com/athlete/russell-westbrook",
+                "https://www.biography.com/athlete/derrick-rose","https://www.biography.com/athlete/kevin-durant","https://www.biography.com/athlete/dwyane-wade","https://www.biography.com/athlete/kobe-bryant","https://www.biography.com/athlete/lebron-james",
+                "https://www.biography.com/athlete/magic-johnson","https://www.biography.com/athlete/miro-jurisic","https://www.biography.com/athlete/kareem-abdul-jabbar","https://www.biography.com/athlete/shaquille-oneal","https://www.biography.com/athlete/chris-paul"};
+        for(int x = 0; x < 15 ; x++ ){
+            list.add(new Player(name[x], age[x], worth[x], "Basketball", imageResource[x], url[i]));
+        }
+    }
 }
